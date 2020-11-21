de904a2bdfc9cd05e1824d98b0e47ac259bc500c
Joey Sisk <jjmsisk@gmail.com>
2020-11-20 20:39:14 -0600

    working js

--- script.js
+++ script.js
@@ -6,38 +6,43 @@ let savedCities = { // fill out the city buttons.
   city3: ""
 };
 
-function buildQueryURL() { // build the first API url for the current day
+let cityBtn;
+
+function updatePage() { // load data to page and fill API's
 
   const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${$("#searchCity").val().trim()}&appid=4535e384f0a4ea4ebc02ddb6fb1569c5&units=imperial`;
 
   console.log("---------------\nBasic URL: " + queryURL + "\n---------------");
 
-  return queryURL;
-}
-
-function updatePage(weatherData) { // load data to page and fill out UV and forcast API's
-
-  $("#mainCity").text(weatherData.name).append(` <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png"></img>`);
-
-  $("#mainTemp").text("Temperature: " + weatherData.main.temp);
-
-  $("#mainHumidity").text("Humidity: " + weatherData.main.humidity);
-
-  $("#mainWind").text("Wind speed: " + weatherData.wind.speed);
-
-  // UV API
-
-  const UVQueryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=4535e384f0a4ea4ebc02ddb6fb1569c5`;
-
-  console.log("---------------\nUV URL: " + UVQueryURL + "\n---------------");
-
   $.ajax({
-    url: UVQueryURL,
+    url: queryURL,
     method: "GET"
-  }).then(function (UVWeatherData) {
+  }).then(function (weatherData) {
 
-    $("#mainUV").text("UV Index: " + UVWeatherData.value);
+    $("#fiveDayCards").empty(); // clear out forcast cards
 
+    $("#mainCity").text(weatherData.name).append(` <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png"></img>`);
+
+    $("#mainTemp").text("Temperature: " + weatherData.main.temp);
+
+    $("#mainHumidity").text("Humidity: " + weatherData.main.humidity);
+
+    $("#mainWind").text("Wind speed: " + weatherData.wind.speed);
+
+    // UV API
+
+    const UVQueryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=4535e384f0a4ea4ebc02ddb6fb1569c5`;
+
+    console.log("---------------\nUV URL: " + UVQueryURL + "\n---------------");
+
+    $.ajax({
+      url: UVQueryURL,
+      method: "GET"
+    }).then(function (UVWeatherData) {
+
+      $("#mainUV").text("UV Index: " + UVWeatherData.value);
+
+    });
   });
 
   // forcast API
@@ -72,8 +77,6 @@ function updatePage(weatherData) { // load data to page and fill out UV and forc
 
       let forcastDate = forcastWeatherData.list[i * 8 + 3].dt_txt;
 
-
-
       $(`${'#date' + i}`).text(forcastDate.slice(5, 10));
 
       $(`${'#icon' + i}`).append(`<img src="http://openweathermap.org/img/wn/${forcastWeatherData.list[i * 8 + 3].weather[0].icon}.png"></img>`);
@@ -85,6 +88,8 @@ function updatePage(weatherData) { // load data to page and fill out UV and forc
     }
 
   });
+
+  saveCities();
 
 }
 
@@ -99,11 +104,11 @@ function checkStorage() { // load previously saved cities
 
 function saveCities() { // save most recent search and move each item down one
 
-  savedCities[3] = savedCities[2];
-  savedCities[2] = savedCities[1];
-  savedCities[1] = savedCities[0];
-  savedCities[0] = $("#searchCity").val().trim();
-  
+  savedCities["city3"] = savedCities["city2"];
+  savedCities["city2"] = savedCities["city1"];
+  savedCities["city1"] = savedCities["city0"];
+  savedCities["city0"] = $("#searchCity").val().trim();
+
   localStorage.setItem("cities", JSON.stringify(savedCities));
 
   printCities();
@@ -112,32 +117,33 @@ function saveCities() { // save most recent search and move each item down one
 
 function printCities() { // print cities from storage to the stage
 
-  // $("#city0").text("Boston");
+  $("#city0").text(savedCities.city0);
+  $("#city1").text(savedCities.city1);
+  $("#city2").text(savedCities.city2);
+  $("#city3").text(savedCities.city3);
 
-
-  for (let i = 0; i < 4; i++) {
-    const citySpot = "city" + i;
-
-    console.log("---------------\ncitySpot: " + savedCities.citySpot + "\n---------------");
-
-    $(`${'#city' + i}`).text(savedCities.citySpot);
-    
-  }
 }
 
 $("#runSearch").on("click", function (event) { // when user enters city in textbox
   event.preventDefault();
 
-  $("#fiveDayCards").empty();
+  updatePage();
 
-  const queryURL = buildQueryURL();
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(updatePage);
-
-  saveCities();
 });
 
+$(".cityBtn").on("click", function (event) {
+  event.preventDefault();
+
+  cityBtn = $(this).text();
+
+  console.log("---------------\nThis Buttom: " + cityBtn + "\n---------------");
+
+  $("#searchCity").val(cityBtn);
+
+  updatePage();
+
+})
+
 checkStorage(); // check local storage
+
+printCities();
